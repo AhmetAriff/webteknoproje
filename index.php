05b3316447c351f079aa913d4f30e68d04dce911
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2022-05-04 22:14:45 +0300

    index.php ile form elemanlarına girilen degerlerin görüntülenmsei

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+  
+   echo"AD:".$_POST['ad']."<br><br>";
+   echo"SOYAD:".$_POST['soyad']."<br><br>";
+   echo"TELEFON NO:".$_POST['telno']."<br><br>";
+   echo"E-MAİL ADRESİ:".$_POST['e-posta']."<br><br>";
+   echo"ADRES:".$_POST['adres']."<br><br>";
+   echo"CİNSİYET:".$_POST['cinsiyet']."<br><br>";
+   echo"İLETİLEN MESAJ:".$_POST['yazialani']."<br><br>";
+   echo "-------------------------------------------------------------------------------" ;  
+ 
+?>
