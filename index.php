f7265b56f86519f322f03247d75768175748100c
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2022-05-12 01:50:10 +0300

    hakkımda sayfası yapımı  iletişim sayfası düzenlenmesi

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
    echo"TELEFON NO:".$_POST['telno']."<br><br>";
    echo"E-MAİL ADRESİ:".$_POST['e-posta']."<br><br>";
    echo"ADRES:".$_POST['adres']."<br><br>";
-   echo"CİNSİYET:".$_POST['cinsiyet']."<br><br>";
    echo"İLETİLEN MESAJ:".$_POST['yazialani']."<br><br>";
    echo "---------------------------------------------------------------------------" ;  
  
