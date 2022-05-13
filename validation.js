ecb7a01e4f8477a18cddca28585529098a4fca5c
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2022-05-14 01:43:29 +0300

    form validation tamamlandi

--- validation.js
+++ validation.js
@@ -3,21 +3,21 @@ function validateForm() {
   let soyisim = document.forms["form"]["SOYAD"].value;
   let telno = document.forms["form"]["TELEFON NO"].value;
   let mail = document.forms["form"]["E-MAİL ADRESİ"].value;
+  var telValidation =
+    /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  var mailValidation =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
   if (isim == "" || soyisim == "" || telno == "" || mail == "") {
     alert("İsim,soyisim,telefon no,mail adresi kısmı boş bırakılamaz");
     return false;
-  }
-}
-
-function validateMail() {
-  let mail = document.forms["form"]["E-MAİL ADRESİ"].value;
-  var validRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-  if (mail.value.match(validRegex)) {
-    alert("invalid email address!");
-
+  } else if (!telValidation.test(telno)) {
+    alert("Geçerli bir telefon numarası giriniz");
+    return false;
+  } else if (!mailValidation.test(mail)) {
+    alert("geçerli bir mail adresi giriniz");
+    return false;
+  } else {
     return true;
   }
 }
